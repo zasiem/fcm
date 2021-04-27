66717b1c9324bf1ba2c4ff9aae3e97487609cb6b
Erza Putra Albasori <erzaputra9110@gmail.com>
2021-04-27 13:50:28 +0700

    fix: gradle version

--- app/src/main/java/com/example/fcm/MainActivity.kt
+++ app/src/main/java/com/example/fcm/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.fcm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.FirebaseApp
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        FirebaseApp.initializeApp(this)
     }
 }
